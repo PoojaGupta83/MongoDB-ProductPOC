9081040ded1a725a2496330c6dbedf948c4d45a8
PoojaGupta83 <poojaguptaa393@gmail.com>
2025-08-02 23:58:49 +0530

    create custom Exception class and create constant class

--- src/main/java/com/product/management/constant/ProductException.java
+++ src/main/java/com/product/management/constant/ProductException.java
@@ -1,4 +1,5 @@
 package com.product.management.constant;
 
 public class ProductException {
+    public static final String  UNABLE_TO_FIND_PRODUCT="Unable to Find product with the given id";
 }
--- src/main/java/com/product/management/exception/ProductException.java
+++ src/main/java/com/product/management/exception/ProductException.java
@@ -1,4 +1,11 @@
 package com.product.management.exception;
 
-public class ProductException {
+import static com.product.management.constant.ProductException.UNABLE_TO_FIND_PRODUCT;
+
+public class ProductException extends RuntimeException{
+    private String id;
+
+    public ProductException(String id){
+        super(UNABLE_TO_FIND_PRODUCT+id);
+    }
 }
