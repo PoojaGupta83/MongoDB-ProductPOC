640baf6455f699e95b6b3b3cc010f644db745754
PoojaGupta83 <poojaguptaa393@gmail.com>
2025-08-02 23:28:49 +0530

    remove springfox and replace it with springdoc-openapi

--- src/main/java/com/product/management/configuration/SwaggerConfig.java
+++ src/main/java/com/product/management/configuration/SwaggerConfig.java
@@ -1,17 +1,20 @@
 package com.product.management.configuration;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+
+    public OpenAPI productPocOenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                .title("MongoDB Product POC API")
+                .description("SpringBoot MongoDB POC")
+                .version("v1.0").license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                                .externalDocs(new ExternalDocumentation().description("Github Repository").url("https://github.com/MongoPoc"));
     }
 }
